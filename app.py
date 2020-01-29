b262e9cefac4adfcc56f3d959d7812d752936f80
hronorog <hronorog@gmail.com>
2020-01-30 01:35:29 +0300

    hard way to hell

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+# -*- coding: utf8 -*-
 from flask import Flask, render_template
+from data import *
+from random import shuffle
 
 
 app = Flask(__name__)
@@ -6,17 +9,44 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    # генерируем 6 случайных отелей на главной странице
+    lst = [x for x in range(1, 17)]
+    shuffle(lst)
+    lst = lst[:6]
+    spisok = []
+    for i in lst:
+        spisok.append(tours.get(i))
+
+    return render_template('index.html',
+                           check_cards=True,
+                           title=title,
+                           tours=spisok,
+                           subtitle=subtitle,
+                           description=description,
+                           departures=departures)
 
 
 @app.route('/from/<direction>')
 def direction(direction):
-    return render_template('direction.html')
+
+    lst = []
+    for k, v in tours.items():
+        if v["departure"] == "kazan":
+            lst.append(v)
+
+    return render_template('direction.html',
+                           check_cards=True,
+                           title=title,
+                           tours=lst,
+                           subtitle=subtitle,
+                           description=description,
+                           departures=departures
+                           )
 
 
-@app.route('/tours/<id>')
+@app.route('/<id>')
 def tour(id):
-    return render_template('tour.html')
+    return render_template('tour.html', toursid=tours)
 
 
 @app.errorhandler(404)
